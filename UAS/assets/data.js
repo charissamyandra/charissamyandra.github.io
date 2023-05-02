de9b8420a7f684a8d905ee540f6a73937b1fbe94
charissamyandra <124018363+charissamyandra@users.noreply.github.com>
2023-05-02 10:37:54 +0700

    Add files via upload

---
+++ UAS/assets/data.js
@@ -0,0 +1,92 @@
+var item = [
+  {
+    name: "Bucket Hat",
+    sku: 1,
+    price: "Rp 499,000",
+    category: "Accessories",
+    status: "Sale 50%",
+    foto: "Hat1.jpg"
+  },
+  
+  {
+      name: "Cross-body bag (Small, 1L)",
+      sku: 2,
+      price: "Rp 369,000",
+      category: "Accessories",
+      status: "Sale 50%",
+      foto: "Bag1.jpg"
+    },
+    
+    {
+      name: "Retro Duffle Bag (13L)",
+      sku: 3,
+      price: "Rp. 539,000",
+      category: "Accessories",
+      status: "Best Seller",
+      foto: "Bag2.jpg"
+    },
+
+    {
+      name: "Jordan 23 Engineered Women's Diamond Woven Trousers",
+      sku: 4,
+      price: "Rp. 1,659,000",
+      category: "Bottom",
+      status: "Best Seller",
+      foto: "Bottom1.jpg"
+    },
+
+    {
+      name: "Men's Cropped Trousers",
+      sku: 5,
+      price: "Rp. 698,000",
+      category: "Bottom",
+      status: "Best Seller",
+      foto: "Bottom2.jpg"
+    },
+
+    {
+      name: "Women's High-Rise Woven Cargo Trousers",
+      sku: 6,
+      price: "Rp. 899,000",
+      category: "Bottom",
+      status: "Sale 50%",
+      foto: "Bottom3.jpg"
+    },
+
+    {
+      name: "F.C. Barcelona Women's Nike Football T-Shirt",
+      sku: 7,
+      price: "Rp. 369,000",
+      category: "Top",
+      status: "Best Seller",
+      foto: "Top1.jpg"
+    },
+
+    {
+      name: "Men's T-shirt",
+      sku: 8,
+      price: "Rp. 499,000",
+      category: "Top",
+      status: "Sale 50%",
+      foto: "Top2.jpg"
+    },
+
+    {
+      name: "Men's T-shirt",
+      sku: 9,
+      price: "Rp. 499,000",
+      category: "Top",
+      status: "Sale 50%",
+      foto: "Top3.jpg"
+    },
+
+    {
+      name: "Women's Long-Sleeve Running Top",
+      sku: 10,
+      price: "Rp. 1,169,000",
+      category: "Top",
+      status: "Best Seller",
+      foto: "Top4.jpg"
+    }
+  ]
+    
